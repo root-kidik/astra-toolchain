956e3150615b9e62c17e6f84c4aa51fddb406863
Nikita <rtkid.nik@gmail.com>
2024-08-19 00:22:19 +0700

    fix: `init_postgresql.sh`

--- intermediate/init_postgresql.sh
+++ intermediate/init_postgresql.sh
@@ -1,14 +1,12 @@
 TOOLCHAIN_DIR=/toolchain
+POSTGRES_USER=postgres
 
 mkdir $TOOLCHAIN_DIR/usr/local
 mkdir $TOOLCHAIN_DIR/usr/local/pgsql
 mkdir $TOOLCHAIN_DIR/usr/local/pgsql/data
-adduser postgres
+
+useradd -p 1234 -m $POSTGRES_USER
 chown postgres $TOOLCHAIN_DIR/usr/local/pgsql/data
-su - postgres
 
-TOOLCHAIN_DIR=/toolchain
-
-$TOOLCHAIN_DIR/usr/bin/initdb -D $TOOLCHAIN_DIR/usr/local/pgsql/data
-$TOOLCHAIN_DIR/usr/bin/pg_ctl -D $TOOLCHAIN_DIR/usr/local/pgsql/data -l logfile start
-$TOOLCHAIN_DIR/usr/bin/createdb test
+su -c '$TOOLCHAIN_DIR/usr/bin/initdb -D $TOOLCHAIN_DIR/usr/local/pgsql/data' $POSTGRES_USER
+su -c 'cd ~ && $TOOLCHAIN_DIR/usr/bin/pg_ctl -D $TOOLCHAIN_DIR/usr/local/pgsql/data -l logfile start' $POSTGRES_USER
